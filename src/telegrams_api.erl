b82f4468c6dfb19a303be385fcf3f2506404d1fd
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-01-19 00:41:46 +0100

    Implement a basic chan pubsub

--- src/telegrams_api.erl
+++ src/telegrams_api.erl
@@ -20,11 +20,26 @@ dispatch(Req) ->
             publish(Channel, Req2)
     end.
 
-subscribe(_Channel, Req) ->
-    {ok, Req}.
+subscribe(Channel, Req) ->
+    {ok, Chan} = telegrams_chan:chan(Channel),
+    ok = telegrams_chan:subscribe(Chan, self()),
+    {ok, Req2} = cowboy_req:chunked_reply(200, Req),
+    stream(Req2).
 
-publish(_Channel, Req) ->
-    {ok, Req}.
+stream(Req) ->
+    receive
+        {event, Event} ->
+            ok = cowboy_req:chunk(Event, Req),
+            stream(Req);
+        _ ->
+            stream(Req)
+    end.
+
+publish(Channel, Req) ->
+    {ok, Chan} = telegrams_chan:chan(Channel),
+    {ok, Payload, Req2} = cowboy_req:body(Req),
+    ok = telegrams_chan:push(Chan, Payload),
+    cowboy_req:reply(200, [], <<>>, Req2).
 
 terminate(_Req, _State) ->
     ok.
---
+++ src/telegrams_chan.erl
@@ -0,0 +1,48 @@
+-module(telegrams_chan).
+-behaviour(gen_server).
+
+-export([chan/1, push/2, subscribe/2]).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
+
+
+chan(_Name) ->
+    case whereis(channel) of
+        undefined ->
+            {ok, Chan} = gen_server:start_link(?MODULE, [], []),
+            register(channel, Chan),
+            {ok, Chan};
+        Chan ->
+            {ok, Chan}
+    end.
+
+push(Chan, Event) ->
+    gen_server:call(Chan, {push, Event}).
+
+subscribe(Chan, Subscriber) ->
+    gen_server:call(Chan, {subscribe, Subscriber}).
+
+
+init([]) ->
+    {ok, []}.
+
+handle_call({push, Event}, _From, Subscribers) ->
+    [Subscriber ! {event, Event} || Subscriber <- Subscribers],
+    {reply, ok, Subscribers};
+handle_call({subscribe, Subscriber}, _From, Subscribers) ->
+    {reply, ok, [Subscriber|Subscribers]}.
+
+handle_cast(_, State) ->
+    {noreply, State}.
+
+handle_info(Msg, State) ->
+    io:format("Unexpected message: ~p~n",[Msg]),
+    {noreply, State}.
+
+terminate(normal, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
