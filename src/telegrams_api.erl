76ecb96e69bfd463999f2c4522504980104f14bb
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-01-18 23:43:04 +0100

    Implement a basic dispatch

--- src/telegrams_api.erl
+++ src/telegrams_api.erl
@@ -8,8 +8,23 @@ init({tcp, http}, Req, _Opts) ->
     {ok, Req, undefined}.
 
 handle(Req, State) ->
-    {ok, Req2} = cowboy_req:reply(200, [], <<"Hello World!">>, Req),
+    {ok, Req2} = dispatch(Req),
     {ok, Req2, State}.
+
+dispatch(Req) ->
+    {Channel, Req2} = cowboy_req:path(Req),
+    case cowboy_req:method(Req2) of
+        {<<"GET">>, Req2} ->
+            subscribe(Channel, Req2);
+        {<<"POST">>, Req2} ->
+            publish(Channel, Req2)
+    end.
+
+subscribe(_Channel, Req) ->
+    {ok, Req}.
+
+publish(_Channel, Req) ->
+    {ok, Req}.
 
 terminate(_Req, _State) ->
     ok.
